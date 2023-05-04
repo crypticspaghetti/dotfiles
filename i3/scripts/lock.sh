77d3fc4f07da179fbd0d275a27f3e7d428edbfa2
Cryptic Spaghetti <crypticspaghetti@protonmail.com>
2023-05-04 15:35:00 -0600

    Add i3 config

---
+++ i3/scripts/lock.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+img=/tmp/i3lock.png
+
+scrot $img
+convert $img -scale 10% -scale 1000% $img
+
+i3lock -u -i $img
+
--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@
 mkdir -p "$HOME/.config/nvim/undo"
 ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 
+# i3
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+
 rm -rf "$HOME/.config/X11"
 ln -sf "$HOME/dotfiles/X11" "$HOME/.config"
 
