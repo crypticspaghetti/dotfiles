09ced1953ad8dc03efa3003dd466dad215440420
Cryptic Spaghetti <crypticspaghetti@protonmail.com>
2023-05-04 16:19:49 -0600

    Add Zsh config

--- install.sh
+++ install.sh
@@ -8,6 +8,11 @@ ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
 
+# zsh
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+
 rm -rf "$HOME/.config/X11"
 ln -sf "$HOME/dotfiles/X11" "$HOME/.config"
 
